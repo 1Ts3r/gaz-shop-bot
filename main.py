368763622bec8796b890e89215c3ab753f404337
Oleksandr <countryballsmapping@gmail.com>
2023-06-24 12:39:46 +0300

    Add base bot logig

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ def error(message):
 if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    server.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
