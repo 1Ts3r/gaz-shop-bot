7f8fc426f86b581fdd23b13df23b1b6468b64439
Oleksandr <countryballsmapping@gmail.com>
2023-06-24 12:46:26 +0300

    Add base bot logig

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 TOKEN = '5814137876:AAFakytbuSZ0NU8XBxgcrRublNqEJDbH2JI'
-APP_URL = 'https://shopbot-e8f46ec4abdb.herokuapp.com/' + TOKEN
+APP_URL = 'https://gazshopbot-0da364ee6e14.herokuapp.com/' + TOKEN
